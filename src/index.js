53aafed11d8ac01822dcb4c5b9b12b4a24085acd
yash <yashsaundalkar0@gmail.com>
2025-05-02 11:34:39 +0530

    ssdfs

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/to-do-list">
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+  <App />
+</HashRouter>
   </React.StrictMode>
 );
 
