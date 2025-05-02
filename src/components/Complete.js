3985acd7ca64c13f529934957a5038eda726c09e
yash <yashsaundalkar0@gmail.com>
2025-05-02 11:27:45 +0530

    sss

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -3,30 +3,31 @@ import React from 'react';
 function Complete({ completedTasks, onDelete }) {
   return (
     <div>
-    <h5>Completed Tasks</h5>
-    {completedTasks.length === 0 ? (
-      <div className="d-flex justify-content-center align-items-center flex-column" style={{ height: '80vh' }}>
-      <img style={{ width: '30%' }} alt="task img" src="task.jpg" />
-      <p className="text-muted mt-3">You haven’t completed any tasks yet.</p>
-    </div>    
-    ) : (
-      completedTasks.map(task => (
-        <div key={task.id} className="card mb-2">
-          <div className="card-body">
-            <h6 className='title'>
-            {task.title}{' '}
-            <small className="float-end">{task.date}</small>
-            </h6>
-            <p style={{ whiteSpace: 'pre-line' }}>{task.note}</p>
-            <button className="btn btn-danger btn-sm" onClick={() => onDelete(task.id)}>
-              Delete
-            </button>
-          </div>
+      <h5>Completed Tasks</h5>
+      {completedTasks.length === 0 ? (
+        <div className="d-flex justify-content-center align-items-center flex-column" style={{ height: '80vh' }}>
+          <img style={{ width: '30%' }} alt="task img" src="task.jpg" />
+          <p className="text-muted mt-3">You haven’t completed any tasks yet.</p>
         </div>
-      ))
-    )}
-  </div>
-  
+      ) : (
+        completedTasks.map(task => (
+          <div key={task.id} className="card mb-2">
+            <div className="card-body">
+              <h6 className='title'>
+                {task.title}{' '}
+                <small className="float-end">{task.date}</small>
+              </h6>
+              <p style={{ whiteSpace: 'pre-line' }}>{task.note}</p>
+              <button className="btn btn-danger btn-sm" onClick={() => onDelete(task.id, 'complete')}>
+                Delete
+              </button>
+
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+
   );
 }
 
