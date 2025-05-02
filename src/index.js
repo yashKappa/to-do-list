e3f761bf996daaaf6ac6cfb05faaeeb4ac2fded7
yash <yashsaundalkar0@gmail.com>
2025-05-02 11:10:57 +0530

    dd

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/to-do-list">
-      <App />
+       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
